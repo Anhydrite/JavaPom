3a8bc4d922a8bdb0d6b71e5ae6bcea93713b7d55
Anhydrite <robinzmuda@gmail.com>
2020-10-23 10:46:00 +0200

    oui

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,129 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import java.util.Set;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+
+	private static Instant startedAt;
+
+	private Calculator calculatorUnderTest;
+
+	@BeforeEach
+	public void initCalculator() {
+		System.out.println("Appel avant chaque test");
+		calculatorUnderTest = new Calculator();
+	}
+
+	@Test
+	void testAddTwoPositiveNumbers() {
+		// Arrange
+		int a = 2;
+		int b = 3;
+		Calculator calculator = new Calculator();
+
+		// Act
+		int somme = calculator.add(a, b);
+
+		// Assert
+		assertEquals(5, somme);
+
+		long factor;
+		factor = calculator.factorielle(10);
+
+		assertEquals(3628800, factor);
+	}
+
+	public void multiply_shouldReturnTheProduct_ofTwoIntegers() {
+		// Arrange
+		int a = 42;
+		int b = 11;
+
+		// Act
+		long produit = calculatorUnderTest.factorielle(10);
+
+		// Assert
+		assertEquals(3628800, produit);
+
+	}
+
+	@AfterEach
+	public void undefCalculator() {
+		System.out.println("Appel après chaque test");
+		calculatorUnderTest = null;
+	}
+
+	@BeforeAll
+	static public void initStartingTime() {
+		System.out.println("Appel avant tous les tests");
+		startedAt = Instant.now();
+	}
+
+	@AfterAll
+	static public void showTestDuration() {
+		System.out.println("Appel après tous les tests");
+		Instant endedAt = Instant.now();
+		long duration = Duration.between(startedAt, endedAt).toMillis();
+		System.out.println(MessageFormat.format("Durée des tests : {0} ms", duration));
+	}
+
+	@ParameterizedTest(name = "{0} x 0 doit être égal à 0")
+	@ValueSource(ints = { 1, 2, 42, 1011, 5089 })
+	public void multiply_shouldReturnZero_ofZeroWithMultipleIntegers(int arg) {
+		// Arrange -- Tout est prêt !
+		// Act -- Multiplier par zéro
+		int actualResult = calculatorUnderTest.multiply(arg, 0);
+		// Assert -- ça vaut toujours zéro !
+		assertEquals(0, actualResult);
+	}
+
+	@ParameterizedTest(name = "{0} + {1} should equal to {2}")
+	@CsvSource({ "1,1,2", "2,3,5", "42,57,99" })
+	public void add_shouldReturnTheSum_ofMultipleIntegers(int arg1, int arg2, int expectResult) {
+		// Arrange -- Tout est prêt !
+		// Act
+		int actualResult = calculatorUnderTest.add(arg1, arg2);
+		// Assert
+		assertEquals(expectResult, actualResult);
+	}
+
+
+	@Timeout(2)
+	@Test
+	public void longCalcul_shouldComputeInLessThan1Second() {
+		// Arrange
+		// Act
+		calculatorUnderTest.longCalculation();
+		// Assert
+		// ...
+	}
+
+	@Test
+	public void listDigits_shouldReturnsTheListOfDigits_ofPositiveInteger() {
+		// GIVEN
+		int number = 95897;
+		// WHEN
+		Set<Integer> actualDigits = calculatorUnderTest.digitsSet(number);
+		// THEN
+		assertThat(actualDigits).containsExactlyInAnyOrder(5, 7, 8, 9);
+	}
+
+
+
+}
